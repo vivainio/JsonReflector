f3664e7c9d2198a40203a0d3c81bc0d442f2fa3a
Ville M. Vainio <ville.vainio@basware.com>
2020-12-12 21:17:26 +0200

    nested complex type

--- JsonReflector.Test/Program.cs
+++ JsonReflector.Test/Program.cs
@@ -5,9 +5,13 @@ using System.Text.Json;
 
 namespace JsonReflector.Test
 {
+    public class NestedType
+    {
+        public ICollection<string> Whoa { get; set; }
+    }
     public class PageObject
     {
-        public string TargetMethod(int a, string b, List<string> lstring, int[] c)
+        public string TargetMethod(int a, string b, List<string> lstring, int[] c, NestedType complex)
         {
             return "ok";
         }
@@ -20,9 +24,7 @@ namespace JsonReflector.Test
         {
             var m = Dispatcher.ResolveMethod("JsonReflector.Test.PageObject,JsonReflector.Test", "TargetMethod");
 
-
-
-            var s2 = System.Text.Encoding.UTF8.GetBytes(@" ['hello', 1, '12', ['nested'], [2,3]".Replace('\'', '"'));
+            var s2 = System.Text.Encoding.UTF8.GetBytes(@" ['hello', 1, '12', ['nested'], [2,3], { 'Whoa' : ['deep value 1', 'deep2'] } ".Replace('\'', '"'));
             var rd = new Utf8JsonReader(s2);
             rd.Read(); // startarray
             rd.Read(); // function
